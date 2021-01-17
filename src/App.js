60c3265cb8b89482783b600cc561cf2f2acdd5cd
Arthur Vasilev <l0v4iy@yandex.ru>
2021-01-17 20:02:02 +0300

    added hw1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { currentPage: 'map' };
+
+  changePage = (page) => {
+    this.setState({ currentPage: page });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header changePage={(page) => this.changePage(page)} />
+        {this.state.currentPage}
+      </div>
+    );
+  }
 }
 
 export default App;
